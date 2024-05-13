a5122b015954778e4a02e1d12ee308680f428331
a925987 <a925987@192.168.1.20>
2024-05-13 12:10:16 +0200

    gestor y controlador hilo

--- src/main/java/com/example/demo/model/persist/HiloDAO.java
+++ src/main/java/com/example/demo/model/persist/HiloDAO.java
@@ -1,11 +1,13 @@
 package com.example.demo.model.persist;
-
+	
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.entity.Hilo;
 
 @Repository
 public interface HiloDAO extends JpaRepository<Hilo, Integer>{
-
+	@Query(value = "SELECT * FROM Hilo h WHERE h.id_producto = ?1 AND id_hilo = ?2")
+	public Hilo buscarHilo(int id_producto, int id_hilo);
 }
