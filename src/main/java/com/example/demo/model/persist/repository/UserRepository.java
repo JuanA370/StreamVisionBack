cb5fb63582fb3fe7668184c09e722afd6bcd3e36
a926431 <a926431@10.234.57.64>
2024-05-23 11:34:34 +0200

    Commit 23/05/2024 11:30

--- src/main/java/com/example/demo/model/persist/repository/UserRepository.java
+++ src/main/java/com/example/demo/model/persist/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.persist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.demo.model.entities.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+	Optional<UserEntity> findByUsername(String username);
 
 }
