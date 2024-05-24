1177f2a42cddca75f3f8fc7ff750db404bd876d2
a925704 <a925704@10.234.57.118>
2024-05-24 12:30:30 +0200

    Implementación de servicio de json

--- src/main/java/com/example/demo/model/persist/dao/FavoriteDao.java
+++ src/main/java/com/example/demo/model/persist/dao/FavoriteDao.java
@@ -2,12 +2,13 @@ package com.example.demo.model.persist.dao;
 
 import java.util.List;
 
+import com.example.demo.model.dto.InteractDto;
 import com.example.demo.model.entities.Favorite;
 import com.example.demo.model.entities.Product;
 
 public interface FavoriteDao {
 	
-	public Favorite createFavorite(Product product, Long logeduSerId);
+	public Favorite createFavorite(InteractDto interactDto, String token);
 	
 	public List<Product> readFavoriteProductsByUserId(Long logeduSerId);
 	
