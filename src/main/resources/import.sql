0783b6e9b5c751b89583665157fedfcfacc93e0a
A925987-PabloSalazar <salazar.pablo.external@eviden.com>
2024-05-30 16:48:56 +0200

    arreglado el import con los nuevos posts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -27,16 +27,16 @@ INSERT INTO `products` (product_id, tmdb_id, original_title, poster_path, is_fil
 INSERT INTO `products` (product_id, tmdb_id, original_title, poster_path, is_film) VALUES (10, 22980, 'Watch What Happens Live with Andy Cohen', '/onSD9UXfJwrMXWhq7UY7hGF2S1h.jpg', false);
 
 -- -- Posts
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (1, 'Post Title 1', 'Content of post 1', '2023-01-01', 1, 1, NULL);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (2, 'Post Title 2', 'Content of post 2', '2023-01-02', 2, 2, 1);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (3, 'Post Title 3', 'Content of post 3', '2023-01-03', 3, 3, 1);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (4, 'Post Title 4', 'Content of post 4', '2023-01-04', 4, 4, NULL);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (5, 'Post Title 5', 'Content of post 5', '2023-01-05', 5, 5, 4);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (6, 'Post Title 6', 'Content of post 6', '2023-01-06', 6, 6, 4);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (7, 'Post Title 7', 'Content of post 7', '2023-01-07', 7, 7, NULL);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (8, 'Post Title 8', 'Content of post 8', '2023-01-08', 8, 8, 7);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (9, 'Post Title 9', 'Content of post 9', '2023-01-09', 9, 9, 7);
-INSERT INTO `posts` (id, title, content, post_date, product_id, user_id, replied_post_id) VALUES (10, 'Post Title 10', 'Content of post 10', '2023-01-10', 10, 10, NULL);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (1, 1, 'Content of post 1', '2023-01-01', 1, 1);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (2, 2, 'Content of post 2', '2023-01-02', 2, 2);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (3, 3, 'Content of post 3', '2023-01-03', 3, 3);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (4, 4, 'Content of post 4', '2023-01-04', 4, 4);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (5, 5, 'Content of post 5', '2023-01-05', 5, 5);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (6, 6, 'Content of post 6', '2023-01-06', 6, 6);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (7, 7, 'Content of post 7', '2023-01-07', 7, 7);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (8, 8, 'Content of post 8', '2023-01-08', 8, 8);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (9, 9, 'Content of post 9', '2023-01-09', 9, 9);
+INSERT INTO `posts` (id, local_rating, content, post_date, product_id, user_id) VALUES (10, 10, 'Content of post 10', '2023-01-10', 10, 10);
 
 -- -- Purchase
 INSERT INTO `purchases` (user_id, product_id, purchase_date) VALUES (2, 1, '2023-01-01');
