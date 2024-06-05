3f6c209f4121e369e9334061e3b8a7aea59fbde1
erikFREmp <erik.flores.external@eviden.com>
2024-06-05 11:11:33 +0200

    Arreglo de UserDaoImpl

--- src/main/java/com/example/demo/model/dto/UserDto.java
+++ src/main/java/com/example/demo/model/dto/UserDto.java
@@ -11,5 +11,6 @@ public record UserDto (
 	
 	@Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$", message="Password must be 8 characters long and contain a lower case letter, an upper case letter and a number")	
 	String password,
-	String email
+	String email,
+	String language
 ) {}
